dcf407e8fc9d7614df1c61192ed05f51c74fd0b2
Aaron Saksik <aaron.saksik@ynov.com>
2020-05-10 14:49:59 +0200

    error

--- ApiFlask.py
+++ ApiFlask.py
@@ -1,106 +1,93 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 import requests
 import json
 import datetime
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-   return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route("/news")
 def apiNews():
-   return render_template('news.html')
+    return render_template('news.html')
 
-@app.route("/meteo")
-def meteoInit():
-   METEO_API_URL = "http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&appid=86bf118c5132d80d5f656123fe6302db"
-   ville = 'Paris'
-   response = requests.get(METEO_API_URL)
-   content = json.loads(response.content.decode('utf-8'))
-   main = content['main']
 
-   temp = main['temp'] - 273.15
-   temp = round(temp, 2)
-   temp_min = main['temp_min'] - 273.15
-   temp_max = main['temp_max'] - 273.15
-   press = main['pressure']
-   humi = main['humidity']
-   wind = content['wind']['speed'] * 1.792
-
-   wind = round(wind, 2)
-   date = datetime.datetime.now().strftime('%d-%m-%Y')
-   return render_template('meteo.html', ville=ville, main=main, temp=temp, temp_min=temp_min, temp_max=temp_max, press=press, humi=humi, wind=wind, date=date)
-
-@app.route("/meteo", methods=['POST'])
+@app.route("/meteo", methods=['GET', 'POST'])
 def apiMeteo():
-   ville = request.form['ville']
-   METEO_API_URL = "http://api.openweathermap.org/data/2.5/weather?q={},fr&appid=86bf118c5132d80d5f656123fe6302db".format(ville)
-   response = requests.get(METEO_API_URL)
-   content = json.loads(response.content.decode('utf-8'))
-   main = content['main']
+   
+    if request.method == 'GET':
+       ville = request.args.get('name')
+    elif request.method == 'POST':
+       ville = request.form['ville']
+    METEO_API_URL = "http://api.openweathermap.org/data/2.5/weather?q={},fr&appid=86bf118c5132d80d5f656123fe6302db".format(
+        ville)
+    
+    response = requests.get(METEO_API_URL)
+    content = json.loads(response.content.decode('utf-8'))
+    if response.status_code != 200:
+        return render_template("errors/500.html"), 500
 
-   temp = main['temp'] - 273.15
-   temp = round(temp, 2)
-   temp_min = main['temp_min'] - 273.15
-   temp_max = main['temp_max'] - 273.15
-   press = main['pressure']
-   humi = main['humidity']
-   wind = content['wind']['speed'] * 1.792
 
-   wind = round(wind, 2)
-   date = datetime.datetime.now().strftime('%d-%m-%Y')
-   return render_template('meteo.html', ville=ville, main=main, temp=temp, temp_min=temp_min, temp_max=temp_max, press=press, humi=humi, wind=wind, date=date)
+    main = content['main']
 
+    temp = main['temp'] - 273.15
+    temp = round(temp, 2)
+    temp_min = main['temp_min'] - 273.15
+    temp_max = main['temp_max'] - 273.15
+    press = main['pressure']
+    humi = main['humidity']
+    wind = content['wind']['speed'] * 1.792
+    wind = round(wind, 2)
+    date = datetime.datetime.now().strftime('%d-%m-%Y')
+    return render_template('meteo.html', ville=ville, main=main, temp=temp, temp_min=temp_min, temp_max=temp_max, press=press, humi=humi, wind=wind, date=date)      
+
+
+    
+    
 @app.route("/covid-19")
 def covidInit():
-   COVID_API_URL_WORLD = "https://api.covid19api.com/world/total"
-   responseWorld = requests.get(COVID_API_URL_WORLD)
-   contentWorld = json.loads(responseWorld.content.decode('utf-8'))
-   confirme = contentWorld['TotalConfirmed']
-   death = contentWorld['TotalDeaths']
-   recover = contentWorld['TotalRecovered']
-   COVID_API_URL_PAYS = "https://api.covid19api.com/summary"
-   responsePays = requests.get(COVID_API_URL_PAYS)
-   contentPays = json.loads(responsePays.content.decode('utf-8'))
-   countries = contentPays["Countries"]
-   return render_template('covid.html', countries=countries, confirme=confirme, death=death, recover=recover)
-
+    COVID_API_URL_WORLD = "https://api.covid19api.com/world/total"
+    responseWorld = requests.get(COVID_API_URL_WORLD)
+    contentWorld = json.loads(responseWorld.content.decode('utf-8'))
+    confirme = contentWorld['TotalConfirmed']
+    death = contentWorld['TotalDeaths']
+    recover = contentWorld['TotalRecovered']
+    COVID_API_URL_PAYS = "https://api.covid19api.com/summary"
+    responsePays = requests.get(COVID_API_URL_PAYS)
+    contentPays = json.loads(responsePays.content.decode('utf-8'))
+    countries = contentPays["Countries"]
+    return render_template('covid.html', countries=countries, confirme=confirme, death=death, recover=recover)
 
 
 @app.route("/covid-19",  methods=['POST'])
 def apiCovid():
-   COVID_API_URL_WORLD = "https://api.covid19api.com/world/total"
-   responseWorld = requests.get(COVID_API_URL_WORLD)
-   contentWorld = json.loads(responseWorld.content.decode('utf-8'))
-   confirme = contentWorld['TotalConfirmed']
-   death = contentWorld['TotalDeaths']
-   recover = contentWorld['TotalRecovered']
-   COVID_API_URL_PAYS = "https://api.covid19api.com/summary"
-   responsePays = requests.get(COVID_API_URL_PAYS)
-   contentPays = json.loads(responsePays.content.decode('utf-8'))
-   countries = contentPays["Countries"]
-   pays = request.form['pays']
-   date = request.form['date'] + "T00:00:00Z"
-   COVID_API_URL = "https://api.covid19api.com/live/country/{}/status/confirmed/date/{}".format(
+    COVID_API_URL_WORLD = "https://api.covid19api.com/world/total"
+    responseWorld = requests.get(COVID_API_URL_WORLD)
+    contentWorld = json.loads(responseWorld.content.decode('utf-8'))
+    confirme = contentWorld['TotalConfirmed']
+    death = contentWorld['TotalDeaths']
+    recover = contentWorld['TotalRecovered']
+    COVID_API_URL_PAYS = "https://api.covid19api.com/summary"
+    responsePays = requests.get(COVID_API_URL_PAYS)
+    contentPays = json.loads(responsePays.content.decode('utf-8'))
+    countries = contentPays["Countries"]
+    pays = request.form['pays']
+    date = request.form['date'] + "T00:00:00Z"
+    COVID_API_URL = "https://api.covid19api.com/live/country/{}/status/confirmed/date/{}".format(
         pays, date)
-   response = requests.get(COVID_API_URL)
-   content = json.loads(response.content.decode('utf-8'))
-   dataMin = []
-   dataMax = []
-   for i in content:
-      paysTest = i['Country']
-      dataMin.append(paysTest.lower())
-      dataMax.append(paysTest)
-   if pays in dataMin or pays in dataMax :
-        return render_template('covid.html', content=content, pays=pays, countries=countries, confirme=confirme, death=death, recover=recover)
-   else:
-        return render_template("errors/500.html"), 500
+    response = requests.get(COVID_API_URL)
+    content = json.loads(response.content.decode('utf-8'))
+    return render_template('covid.html', content=content, pays=pays, countries=countries, confirme=confirme, death=death, recover=recover)
+   
 
 @app.errorhandler(404)
-def page_not_found(error) :
-   return render_template('errors/404.html'), 404
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
