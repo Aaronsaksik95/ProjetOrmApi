735de2474b3b6a6a7c215449b991eedbc23e0978
Aaron Saksik <aaron.saksik@ynov.com>
2020-05-14 19:08:45 +0200

    a

--- ApiFlask.py
+++ ApiFlask.py
@@ -1,28 +1,44 @@
-from flask import Flask, render_template, url_for, request, jsonify, flash, redirect
+from flask import Flask, render_template, url_for, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_bcrypt import Bcrypt
-from sqlalchemy import Table, Column, Integer, String, MetaData, Date, Text
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import Table, Column, Integer, String, MetaData, Date, Text, ForeignKey
+from sqlalchemy.orm import relationship
+import sqlalchemy as db
 import requests
 import json
 import datetime
-import sqlalchemy as db
+
 
 app = Flask(__name__)
-bcrypt = Bcrypt(app)
-meta = MetaData(app)
+bootstrap = Bootstrap(app)
 app.secret_key = "super secret key"
 login_manager = LoginManager()
 login_manager.init_app(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:8889/projetFlask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:8889/projetTest'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/projetFlask'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+login_manager.login_view = 'login'
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()], render_kw={"placeholder": "Aaron99"})
+    password = PasswordField('Password', validators=[InputRequired()], render_kw={"placeholder": "*******"})
+    remember = BooleanField('remember me')
 
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)], render_kw={"placeholder": "Example@example.com"})
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)], render_kw={"placeholder": "Aaron99"})
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)], render_kw={"placeholder": "*******"})
+    remember = BooleanField('remember me')
 
 
 class Article(db.Model):
+    __tablename__ = 'article'
     id = db.Column(db.Integer, primary_key=True)
     image_article = db.Column(db.Text, nullable=False)
     auteur_article = db.Column(db.String(50), nullable=True)
@@ -31,22 +47,30 @@ class Article(db.Model):
     content_article = db.Column(db.Text, nullable=False)
     date_article = db.Column(db.Date, nullable=False)
     source_article = db.Column(db.String(50), nullable=False)
+    commentaires = relationship('Commentaire', backref='article')
     def __repr__(self):
         return '<Article %r>' % self.id_article
 
 class Commentaire(db.Model):
+    __tablename__ = 'commentaire'
     id = db.Column(db.Integer, primary_key=True)
     content_com = db.Column(db.Text, nullable=False)
     date_com = db.Column(db.Date, nullable=False)
+    users_id = Column(db.Integer, db.ForeignKey('users.id'))
+    article_id = Column(db.Integer, db.ForeignKey('article.id'))
+
     #un commentaire pour un user
     #un article a plusieur commentaire
     def __repr__(self):
         return '<Commentaire %r>' % self.id_com
 
 class Users(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(60), unique=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
+    commentaires = relationship('Commentaire', backref='user')
 
 db.create_all()
 db.session.commit()
@@ -55,54 +79,50 @@ db.session.commit()
 def load_user(user_id):
     return Users.query.get(int(user_id))
 
-@app.route("/login")
-def loginInit():
-    return render_template('login.html')
-
-@app.route("/login", methods=['GET','POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        usernameForm = request.form['username']
-        passwordForm = request.form['password']
-        users = Users.query.filter_by(username=usernameForm).first()
-        if users is None:
-            error = "Aucun compte avec " + usernameForm
-        else:
-            password = users.password
-            if bcrypt.check_password_hash(password, passwordForm):
-                login_user(users)
-                return redirect(url_for('apiNews'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        users = Users.query.filter_by(username=form.username.data).first()
+        if users:
+            if check_password_hash(users.password, form.password.data):
+                login_user(users, remember=form.remember.data)
+                color = "success"
+                flash('Vous êtes bien connecté en tant que ' + form.username.data)
+                return render_template('home.html', color=color)
             else:
-                error = "Le mot de passe est incorrect"
-    return render_template('login.html', error=error)
+                flash('Mot de passe ou identifiant incorrect')
+        else:
+            flash('Mot de passe ou identifiant incorrect')
+    return render_template('login.html', form=form)
 
 @app.route("/logout")
 def logout():
     if current_user.is_authenticated:
         logout_user()
-        return redirect(url_for('login'))
+        color = "danger"
+        flash('Vous vous êtes déconnecté')
+        return render_template('home.html', color=color)
     else: 
         return redirect(url_for('login'))
 
-@app.route("/sign")
-def signInit():
-    return render_template('sign.html')
-
-@app.route("/sign", methods=['POST'])
+@app.route("/sign", methods=['GET', 'POST'])
 def sign():
-    username = request.form['username']
-    password = request.form['password']
-    password  = bcrypt.generate_password_hash( password ) 
-    users = Users.query.filter_by(username=username).first()
-    if users is None:
-        users = Users(username=username, password=password)
-        db.session.add(users)
-        db.session.commit()
-        login_user(users)
-        return render_template('news.html')
-    else:
-        error = "Username déjà utilisé"
-    return render_template('sign.html', error=error)
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        users = Users.query.filter_by(username=form.username.data).first()
+        if users is None:
+            new_user = Users(username=form.username.data, email=form.email.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(users, remember=form.remember.data)
+            return redirect(url_for('home'))
+        else:
+            flash('Identifiant déjà utilisé')
+        
+    return render_template('sign.html', form=form)
 
 @app.route("/")
 def home():
@@ -137,7 +157,7 @@ def apiNews():
 def New():
     idArt = request.args.get('id')
     articleSelect = Article.query.filter_by(id=idArt).first()
-    allCom = Commentaire.query.all()
+    allCom = Commentaire.query.filter(Commentaire.article_id.endswith(idArt)).all()
     allCom = allCom[::-1]
     return render_template('new.html', articleSelect=articleSelect, allCom=allCom)
 
@@ -147,14 +167,14 @@ def Comm():
     if current_user.is_authenticated:
         commentaireForm = request.form['comm']
         date = datetime.datetime.now()
-        commentaire = Commentaire(content_com=commentaireForm, date_com=date)
+        commentaire = Commentaire(content_com=commentaireForm, date_com=date, users_id=current_user.id, article_id=request.args.get('id'))
         db.session.add(commentaire)
         db.session.commit()
     else:
         return redirect(url_for('login'))
     idArt = request.args.get('id')
     articleSelect = Article.query.filter_by(id=idArt).first()
-    allCom = Commentaire.query.all()
+    allCom = Commentaire.query.filter(Commentaire.article_id.endswith(idArt)).all()
     allCom = allCom[::-1]
     return render_template('new.html', articleSelect=articleSelect, allCom=allCom)
 
